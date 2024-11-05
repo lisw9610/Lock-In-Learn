20a80a78636b5f3fad611b7bff14654c40341f07
Sophie Tu <sotu3994@colorado.edu>
2024-11-05 15:07:58 -0700

    updated sql to link courses

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
     username varChar(50) PRIMARY KEY,
-    password varChar(250) NOT NULL,
+    password varChar(250) NOT NULL
 );
 
 CREATE TABLE assignments (
@@ -9,5 +10,13 @@ CREATE TABLE assignments (
     task varChar(250),
     date DATE,
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE user_courses (
+    user_course_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    course_id INT REFERENCES courses(course_id) ON DELETE CASCADE,
+    start_date DATE
+);
+
 SET DATEFORMAT dmy;
