734aea751da2a78515ad5792d977bfb0ad02ff5c
lisw9610 <lisw9610@colorado.edu>
2024-11-19 12:51:24 -0700

    added css for better appearance and added a title to all messages

---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts (title, message, username) VALUES ('First Post', 'First message ever', 'admin');
+
+INSERT INTO users (username, email, password, profile_picture) VALUES ('admin', 'admin@lockinlearn.org', '$2y$10$zWTQlnsiDvPSAgb5Yp90TOIg4K2ac/x.6z8nFvUOOIrtHwpXUipqy', 'https://pbs.twimg.com/profile_images/1455169155733377027/Eczv5-Jb_400x400.jpg')
